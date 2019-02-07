a3069bebaa6e979ea8a4c740b5479b063d2d5338
jcp14@students.ptcollege.edu <>
2019-02-07 11:25:57 -0500

    First commit

---
+++ src/github/Github.java
@@ -0,0 +1,18 @@
+package github;
+
+
+public class Github {
+
+    public static void main(String[] args) {
+        
+        Pekar();
+        
+    }
+    
+    public static void Pekar() {
+        
+        System.out.println("Joey Pekar!");
+        
+    }
+    
+}
